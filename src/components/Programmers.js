dd7d46a0fd7e3cbb608e03bf19a313350988e034
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-06-16 21:14:57 -0700

    Making sure all is up to date

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -36,6 +36,7 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
+    // ??? Need help understanding this code ???
     const id = programmerId-1;
     const name = programmers[id].name
     return name
